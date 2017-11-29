146752aa7b261d356c4a369f171e8971b8cb6e9e
Andre Chaves <andrechavesg@gmail.com>
2017-11-29 17:40:21 -0200

    capitulo 8

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -15,6 +15,25 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
+	private String filtro;
+	
+	public BuscaEmpresa() {
+	    System.out.println("Instanciando uma Servlet do tipo BuscaEmpresa "
+	            + this);
+	}
+	
+	@Override
+	public void init() throws ServletException {
+	    super.init();
+	    System.out.println("Inicializando a Servlet " + this);
+	}
+
+	@Override
+	public void destroy() {
+	    super.destroy();
+	    System.out.println("Destruindo a Servlet " + this);
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	        throws ServletException, IOException {
@@ -24,7 +43,14 @@ public class BuscaEmpresa extends HttpServlet {
 	    writer.println("<body>");
 	    writer.println("Resultado da busca:<br/>");
 
-	    String filtro = req.getParameter("filtro");
+	    filtro = req.getParameter("filtro");
+	    
+	    try {
+	        Thread.sleep(10000);
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+	    
 	    Collection<Empresa> empresas = new EmpresaDAO()
 	            .buscaPorSimilaridade(filtro);
 
